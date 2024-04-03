c031908860e9d8bdd2886189b9c324e2b64c0dae
gcsong023 <66949363+gcsong023@users.noreply.github.com>
2024-04-04 01:52:57 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ command -v wget >/dev/null || {
 }
 
 if [ ! -f "1pctl" ]; then 
-  wget https://github.com/gcsong023/wrt_installer/raw/main/1pctl
+  wget https://raw.githubusercontent.com/gcsong023/wrt_installer/main/1pctl
 fi
 
 if [ ! -f "1panel.service" ]; then 
@@ -14,7 +14,7 @@ if [ ! -f "1panel.service" ]; then
 fi
 
 if [ ! -f "install.sh" ]; then 
-  wget https://github.com/gcsong023/wrt_installer/raw/main/install.sh
+  wget https://raw.githubusercontent.com/gcsong023/wrt_installer/main/install.sh
 fi
 
 chmod 755 1pctl install.sh
